97641d7f0e363b8113168d4d16ad3cd7e52a7c7c
Pablo de la Rosa Michicol <pablo.delarosaml@udlap.com.mx>
2019-09-25 16:17:00 -0500

    Initial Commit

---
+++ KueskiPodTest/KueskiPodTest.h
@@ -0,0 +1,19 @@
+//
+//  KueskiPodTest.h
+//  KueskiPodTest
+//
+//  Created by Pablo de la Rosa Michicol on 9/25/19.
+//  Copyright © 2019 Pablo de la Rosa Michicol. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KueskiPodTest.
+FOUNDATION_EXPORT double KueskiPodTestVersionNumber;
+
+//! Project version string for KueskiPodTest.
+FOUNDATION_EXPORT const unsigned char KueskiPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KueskiPodTest/PublicHeader.h>
+
+
